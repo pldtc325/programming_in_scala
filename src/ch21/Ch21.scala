9aac112367e9c1e8c0ce1d61682357b1b4c650f5
pldtc325 <pldtc325@gmail.com>
2016-08-16 07:37:14 +0800

    add implicit conversion

--- src/ch21/Ch21.scala
+++ src/ch21/Ch21.scala
@@ -1,64 +1,45 @@
 package ch21
 
+import java.awt.event.{ActionEvent, ActionListener}
+import javax.swing.JButton
 
-abstract class ZList[+T] {
-  def isEmpty: Boolean
 
-  def tail: ZList[T]
-
-  def head: T
-
-  def length: Int = if (isEmpty) 0 else 1 + tail.length
+/**
+  * A java Swing example, use inner class to represent a callback
+  * which is tedious, lengthy
+  */
+object ImplicitConversionExample extends App {
+  val button = new JButton
+  button.addActionListener(
+    new ActionListener {
+      def actionPerformed(event: ActionEvent) {
+        println("pressed!")
+      }
+    } )
 
   /**
-    * note that this is recursive function
-    * both 'tail' in the function and n:Int parameter change in each call
+    * scala implicit conversion, turn a function:ActionEvent => Unit
+    * to ActionListener
+    * @param f
+    * @return
     */
-  def drop(n: Int): ZList[T] =
-  if (isEmpty) ZNil
-  else if (n <= 0) this
-  else tail.drop(n - 1) // for each call, tail changes, the sequence would be {tail, tail.tail, ...}
-
-  def map[U](f: T => U): ZList[U] = {
-    if (isEmpty) ZNil
-    else XX(f(head), tail.map(f))
+  implicit def function2ActionListener(f:ActionEvent => Unit) = {
+    new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = f(e)
+    }
   }
 
-  def ::[U >: T](x: U): ZList[U] = {
-    XX(x, this)
-  }
+  /**
+    * scala-way of callback, much more concise
+    * need function -> ActionListener implicit conversion to work
+    */
+  button.addActionListener( // Type mismatch!
+    (_: ActionEvent) => println("pressed!")
+  )
+
 
 }
 
-object ZNil extends ZList[Nothing] {
-  override def isEmpty: Boolean = true
+object Ch21 {
 
-  def tail = throw new NoSuchElementException("tail of empty list")
-
-  def head = throw new NoSuchElementException("head of empty list")
-}
-
-final case class XX[T](h: T, t: ZList[T]) extends ZList[T] {
-  def isEmpty: Boolean = false
-
-  def tail: ZList[T] = t
-
-  def head: T = h
-}
-
-class Fruit
-
-class Apple extends Fruit
-
-class Orange extends Fruit
-
-object Ch21 extends App {
-  //val l:ZList[Int] = XX(1, XX(2, ZNil))
-  //val r = l.drop(2)
-  //println("r:" + r.length)
-
-  val apples = new Apple :: ZNil
-  val o: Orange = new Orange
-  val f1 = new Fruit
-  val f = o :: apples.::(f1)
 }
---
+++ src/ch22/Ch22.scala
@@ -0,0 +1,64 @@
+package ch22
+
+
+abstract class ZList[+T] {
+  def isEmpty: Boolean
+
+  def tail: ZList[T]
+
+  def head: T
+
+  def length: Int = if (isEmpty) 0 else 1 + tail.length
+
+  /**
+    * note that this is recursive function
+    * both 'tail' in the function and n:Int parameter change in each call
+    */
+  def drop(n: Int): ZList[T] =
+  if (isEmpty) ZNil
+  else if (n <= 0) this
+  else tail.drop(n - 1) // for each call, tail changes, the sequence would be {tail, tail.tail, ...}
+
+  def map[U](f: T => U): ZList[U] = {
+    if (isEmpty) ZNil
+    else XX(f(head), tail.map(f))
+  }
+
+  def ::[U >: T](x: U): ZList[U] = {
+    XX(x, this)
+  }
+
+}
+
+object ZNil extends ZList[Nothing] {
+  override def isEmpty: Boolean = true
+
+  def tail = throw new NoSuchElementException("tail of empty list")
+
+  def head = throw new NoSuchElementException("head of empty list")
+}
+
+final case class XX[T](h: T, t: ZList[T]) extends ZList[T] {
+  def isEmpty: Boolean = false
+
+  def tail: ZList[T] = t
+
+  def head: T = h
+}
+
+class Fruit
+
+class Apple extends Fruit
+
+class Orange extends Fruit
+
+object Ch22 extends App {
+  //val l:ZList[Int] = XX(1, XX(2, ZNil))
+  //val r = l.drop(2)
+  //println("r:" + r.length)
+
+  val apples = new Apple :: ZNil
+  val o: Orange = new Orange
+  val f1 = new Fruit
+  val f = o :: apples.::(f1)
+}
