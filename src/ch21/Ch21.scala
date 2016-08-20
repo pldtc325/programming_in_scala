a5ddfd30fa0415e7d656f09a353752eb3e7928e1
pldtc325 <pldtc325@gmail.com>
2016-08-20 23:58:52 +0800

    implicit conversion continue

--- src/ch21/Ch21.scala
+++ src/ch21/Ch21.scala
@@ -55,6 +55,29 @@ object ImplicitConversionExample extends App {
   val f = new F()
   f.m() // would have been a type mismatch if there was not F2E
 
+  /**
+    * implicit parameter
+    */
+  case class MuchMoney()
+  case class LoveYourJob()
+  implicit val loveYourJob = new LoveYourJob
+  def beingHappy(muchMoney: MuchMoney)(implicit loveYourJob: LoveYourJob) { println(muchMoney + " " + loveYourJob)}
+  beingHappy(MuchMoney()) // print MuchMoney() LoveYourJob()
+
+  /**
+    * try to provide information on the type
+    * that is given by previous parameter
+    */
+  trait Detector[T] { def detect(t:T):Boolean}
+  implicit object DetectorInt extends Detector[Int] {
+    override def detect(t: Int): Boolean = t > 100000
+  }
+  def Arrest[T](suspects:List[T])(implicit detector: Detector[T]): List[T] = {
+    suspects filter(detector detect _)
+  }
+  println(Arrest(List(1,100,10000000))) // arrest List(10000000)
+
+
 }
 
 object Ch21 {
